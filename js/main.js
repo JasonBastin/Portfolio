23a45c2e61d59ec127bc0b9876f02f05795ed4fe
Jason Bastin <bastin.jason@gmail.com>
2020-07-08 16:46:48 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuBranding.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach((x) => x.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach((x) => x.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
+
+// Set class='current' on nav-item when clicked
